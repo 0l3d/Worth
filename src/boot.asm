a3194697502a2e6d061d3e5b3a886ad2bd7991e4
0l3d <oled.dev+commit@gmail.com>
2026-06-20 17:57:59 +0300

    framebuffer and next step will be MMU tables

--- src/boot.asm
+++ src/boot.asm
@@ -1,18 +1,18 @@
 #EVERY INSTRUCTION IS 16 BYTE
 section code 
   mov 0, r0 
-  mov 1025, r1 
+  mov 1025, r10 
   outb 9, r0 
 
   mov 0, r3
 getKernelfromdisk:
   
-  inb 10, r2
-  cmp r2, 0x30
+  inb 10, p2b0
+  cmp p2b0, 0x30
   je kernel_end_check 
 
-  store r2, r1 
-  inc r1 
+  store p2b0, r10
+  inc r10
     
   jmp getKernelfromdisk
 
@@ -20,8 +20,8 @@ kernel_end_check:
   inb 9, r0  # PADDING 0x30 ... 7 bytes empty then, imm64 
   add 7, r0  # padding 
   outb 9, r0 
-  inb 10, r2 
-  cmp r2, 10 
+  inb 10, p2b0  
+  cmp p2b0, 10 
   je kernel_start
   
   add 6, r0 
@@ -30,6 +30,6 @@ kernel_end_check:
   jmp getKernelfromdisk
 
 kernel_start:
-  mov r1, r0 # stack pointer set to end 
-  mov 1025, r1 
-  jmp r1 
+  mov r10, r0 # stack pointer set to end 
+  mov 1025, r10
+  jmp r10 
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,60 +1,23 @@
 section code
 entry kernel_start
-stack_overflow:
-  hlt
-
-stack_underflow:
-  hlt 
-
-stack_uoflow_controller:
-  cmp r0, r4
-  jg stack_underflow
-  cmp r0, r3 
-  jl stack_overflow 
-  ret
-
 kernel_start:
-  mov 1010, r10 # kernel start debug 
-  print r10
+  mov 1010, r11 # kernel start debug 
+  print r11
   print r0 # get end memory position coming from bootloader
-
-  # creating a memory layout 
-  # HEAP_START: 
-  mov r0, r1
-  add 2048, r0 
-  mov r0, r2 
-  # HEAP END 
-  # HEAP: r1--between--r2 
-
-  # STACK:
-  mov r0, r3 
-  add 1024, r0 
-  mov r0, r4 
-  # STACK: r3--between--r4 
-  mov r4, r0 # setting stack pointer
-  # now we can use stack
-
-kernel_work: 
-  push r10 
-  mov 120, r10 
-  print r10
-  call stack_uoflow_controller
-  pop r10
-  print r10
 
 kernel_draw: # r10 return val 
   inb 14, r8  # getting framebuffer address 
   inb 15, r7  # framebuffer size
-  print r8 
+  print r8
   print r7
-  mov 255, r6
+  mov 255, p6b0
   mov 0, r9 
 
 loop_frame:
-  cmp r9, r7 
+  cmp r9, r7
   je kernel_halt
   
-  store r6, r8 
+  store p6b0, r8
   inc r9 
   inc r8
   jmp loop_frame
