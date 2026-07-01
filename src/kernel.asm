3bb630aaedb3c9479cce908bc74658c237209954
0l3d <oleddev@gmail.com>
2026-07-01 21:09:15 +0300

    kernel_draw

--- src/kernel.asm
+++ src/kernel.asm
@@ -4,31 +4,16 @@ section code
 kernel_start:
   mov 1010, r11 # kernel start debug 
   print r11
-  print r0 # get end memory position coming from bootloader
 
-  add 1000, r0
-  print r0
-
-  mov 10, r15
-  mov 15, r16
-  mov 20, r17
-  mov 25, r18
+  add 10000, r0
+  
+  mov 200, r16
+  push r16
+  mov 250, r16
+  push r16
+  mov 256, r16
+  push r16
   call kernel_draw
 
-  inb 14, r8  # getting framebuffer address 
-  inb 15, r7  # framebuffer size
-  print r8
-  print r7
-  mov 255, p6b0
-  mov 0, r9 
-loop_frame:
-  cmp r9, r7
-  je kernel_halt
-  
-  store p6b0, r8
-  inc r9 
-  inc r8
-  jmp loop_frame
-
 kernel_halt: 
-  print r9
+  jmp kernel_halt
--- src/syscall.asm
+++ src/syscall.asm
@@ -1,12 +1,46 @@
 section code
 # Arguments:
-# r15 -> Width 
-# r16 -> Height
-# r17 -> X
-# r18 -> Y
+# 1 -> X
+# 2 -> Y
+# 3 -> height
+# All of them must be PUSHED to STACK.
 kernel_draw:
-  print r15 
-  print r16 
-  print r17 
-  print r18
-  ret 
+  pop r11 # function ret pos, we will push it again when using ret
+  pop r18 # getting third argument 
+  pop r17 # getting middle argument 
+  pop r16 # getting first one 
+
+  push r13     # saving user registers (actual memory pos)
+  push r14     # saving user registers (framebuffer start)
+  push r15     # saving user registers (framebuffer size)
+  inb 14, r14  # framebuffer pos 
+  inb 15, r15  # framebuffer size
+  
+  # calculating actual memory pos
+  mov 0, r13 
+  add r14, r13 
+  mul r18, r17
+  print r17
+  add r16, r17
+  print r17
+  add r17, r13 
+  # basically: 
+  # POS(x, y) = y * WINDOW_HEIGHT + x
+  # Memory is a linear block. So we need to calculate actual position of the pixel.
+  # Lets explain the math:
+  # For example: x = 100, y = 50, HEIGHT=256 
+  # (y * HEIGHT) = 50 * 256, that means: we need to go through 256 bytes of memory 50 times.
+  # We ve reached position P, but we re at its beginning
+  # We need to add an offset X to P to write to the desired location. 
+  
+
+  print r13        # debug 
+  mov 255, p15b0   # color (white for test)
+  store p15b0, r13 # writing to memory.
+
+  pop r15      # pop user register
+  pop r14      # pop user register 
+  pop r13      # pop user register.
+
+  push r11     # pushing actual ret adress again for ret.
+  ret          # return 
